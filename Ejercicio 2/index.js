5f07ae049a5415e52bb441eed68f903603ddfc03
ViBaTo <barberatormov@gmail.com>
2023-12-12 11:42:44 +0100

    Ejercicio2

--- Ejercicio 2/index.js
+++ Ejercicio 2/index.js
@@ -9,7 +9,7 @@ document.body.appendChild(myP)
 //! 1.3 Inserta dinámicamente en un html un div que contenga 6 p utilizando un loop con javascript.
 const Div6 = document.createElement('div')
 for (let index = 1; index <= 6; index++) {
-  const nP = document.createElement('p')
+  var nP = document.createElement('p')
   nP.textContent = 'Esto es un nuevo p'
   Div6.appendChild(nP)
 }
@@ -20,12 +20,47 @@ const myNP = document.createElement('div' > 'p')
 myNP.textContent = 'Soy dinámico'
 document.body.appendChild(myNP)
 //! 1.5 Inserta en el h2 con la clase .fn-insert-here el texto 'Wubba Lubba dub dub'.
-
-//! 1.6 Basandote en el siguiente array crea una lista ul > li con los textos del array.
+const myH2 = document.querySelector('h2.fn-insert-here')
+myH2.textContent = 'Wubba Lubba dub dub'
+//! 1.6 Basándote en el siguiente array crea una lista ul > li con los textos del array.
 const apps = ['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter']
 
+/* var ul = document.createElement('ul')
+for (let index = 0; index < apps.length; index++) {
+  const app = apps[index]
+  const li = document.createElement('li')
+  li.textContent = app
+  ul.appendChild(li)
+}
+document.body.appendChild(ul) */
+
+const myUl = document.createElement('ul')
+apps.forEach(function (app) {
+  var myLi = document.createElement('li')
+  myLi.textContent = app
+  myUl.appendChild(myLi)
+})
+
+document.body.appendChild(myUl)
+
 //! 1.7 Elimina todos los nodos que tengan la clase .fn-remove-me
+const removeMe = document.querySelectorAll('.fn-remove-me')
+removeMe.forEach(function (e) {
+  e.remove()
+})
 
 //! 1.8 Inserta una p con el texto 'Voy en medio!' entre los dos div. Recuerda que no solo puedes insertar elementos con .appendChild.
+const p = document.createElement('p')
+p.textContent = 'Voy en medio'
+var divs = document.querySelectorAll('div')
+var div2 = divs[1]
+div2.parentNode.insertBefore(p, div2)
 
 //! 1.9 Inserta p con el texto 'Voy dentro!', dentro de todos los div con la clase .fn-insert-here
+const div9s = document.querySelectorAll('.fn-insert-here')
+
+div9s.forEach(function (div9) {
+  var p = document.createElement('p')
+  p.textContent = 'Voy dentro'
+  div9.appendChild(p)
+})
